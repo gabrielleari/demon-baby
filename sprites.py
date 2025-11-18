b2d8e21c3106e7b859b352f20252e7672d825e00
gabrielleari <gabrielle.ari222@gmail.com>
2025-11-18 14:58:02 -0600

    implemented and scaled down background image

---
+++ sprites.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+class BG(pygame.sprite.Sprite):
+    def __init__(self,groups, scale_factor):
+        super().__init__(groups)
+        bg_image = pygame.image.load('environment/background1.png').convert()
+        
+        #.getheight method
+        full_height = bg_image.get_height() * scale_factor 
+        full_width = bg_image.get_width() * scale_factor
+
+        self.image = pygame.transform.scale(bg_image,(full_width, full_height))
+        self.rect = self.image.get_rect(topleft = (0,0))
