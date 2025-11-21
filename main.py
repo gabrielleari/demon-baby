652a8fc7a0cced848174c8d8c3151ce91e7613b8
gabrielleari <gabrielle.ari222@gmail.com>
2025-11-21 17:25:03 -0600

    added comments + implemented moving background

--- main.py
+++ main.py
@@ -3,43 +3,44 @@ from settings import *
 from sprites import BG
 
 class Game: 
-    def __init__(self):
+    def __init__(self): #function that runs when new object is created
 
         #setup
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-        pygame.display.set_caption('Demon Baby')
-        self.clock = pygame.time.Clock()
+        pygame.init() #turns un/starts all python modules game needs
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT)) #creates game window, sets screen surface
+        pygame.display.set_caption('Demon Baby') #sets title of game window at top of tab
+        self.clock = pygame.time.Clock() #creates clock object that keeps track of time in game
+        self.active = True #
 
         #sprite groups
-        self.all_sprites = pygame.sprite.Group()
-        self.collision_sprites = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group() #creates group to store all game sprites
+        self.collision_sprites = pygame.sprite.Group() #group to store sprites to check collisions against
 
         #scale factor
-        bg_height = pygame.image.load('environment/background1.png').get_height()
-        self.scale_factor = WINDOW_HEIGHT / bg_height
-        print(self.scale_factor)
+        bg_height = pygame.image.load('environment/background1.png').get_height() #loads image and gets height
+        self.scale_factor = WINDOW_HEIGHT / bg_height #calculate scaling factor to resize bg img to fit window height
 
         #sprite setup
-        BG(self.all_sprites, self.scale_factor)
+        BG(self.all_sprites, self.scale_factor) #make bg sprite, scale it to screen, and add to main sprite group so it appears in game
 
-    def run(self):
-        last_time = time.time()
-        while True:
+    def run(self): #func will run main logic of gamw
+        last_time = time.time() #store current time (in seconds) in variable called last_time
+        while True: 
 
-            #delta time
-            dt = time.time() - last_time
-            last_time = time.time()
+            dt = time.time() - last_time #calculate time difference (delta time) since last frame
+            last_time = time.time() #update last_time to current time
 
             #event loop
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pygame.event.get(): #go through all events Pygame has registered since the last game
+                if event.type == pygame.QUIT: #go through all events pygame has registered since last frame
                     pygame.quit()
                     sys.exit()
             #game logic
-            self.all_sprites.draw(self.display_surface)
-            pygame.display.update()
-            self.clock.tick(FRAMERATE)
+            self.display_surface.fill('black') #fills entire game window w black color
+            self.all_sprites.update(dt) #tells all sprites in all sprites group to update themselves
+            self.all_sprites.draw(self.display_surface) #draws all sprites in the all_sprites group onto game window (display_surface)
+            pygame.display.update() #refresh game window to show everything drawn since last frame
+            self.clock.tick(FRAMERATE) #limit game loop to run at maximum number of FPS
 
 if __name__ == '__main__':
         game = Game()
--- sprites.py
+++ sprites.py
@@ -1,19 +1,22 @@
 import pygame
 from settings import *
-
-class BG(pygame.sprite.Sprite):
-    def __init__(self,groups, scale_factor):
-        super().__init__(groups)
-        bg_image = pygame.image.load('environment/background1.png').convert()
+from random import choice, randint
+class BG(pygame.sprite.Sprite): #creates bg object that behaves like pygame sprite
+    def __init__(self,groups, scale_factor): #runs setup: which group it belongs to and how big it should be
+        super().__init__(groups) #sets up bg object as real pygame sprite and add it to the given groups
+        bg_image = pygame.image.load('environment/background1.png').convert() #loads bg image from file and convert it for faster display
         
         #.getheight method
-        full_height = bg_image.get_height() * scale_factor 
-        full_width = bg_image.get_width() * scale_factor
+        full_height = bg_image.get_height() * scale_factor #calculate new height of bg img after scaling
+        full_width = bg_image.get_width() * scale_factor #calculate new width of bg img after scaling
 
-        self.image = pygame.transform.scale(bg_image,(full_width, full_height))
-        self.rect = self.image.get_rect(topleft = (0,0))
-        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.image = pygame.transform.scale(bg_image,(full_width, full_height)) #resize bg img to desired height and width and store in sprite's img attribute
+        self.rect = self.image.get_rect(topleft = (0,0)) #create rectangle for sprite that dedines its position for human interaction,
+        self.pos = pygame.math.Vector2(self.rect.topleft) #stores sprite's position as 2D vector for easier movement and math operations
 
-    def update(sef, dt):
-        self.pos.x -= 300
-        
+    def update(self, dt): #define update method for sprite to change its state every frame
+        self.pos.x -= 300 * dt #moves sprite left at speed of 300 px per second, scaled by time passed in sec since last frame
+        self.rect.x = round(self.pos.x) #updates sprites rectangle position to match Vector2 position, rounding to an integer
+        if self.rect.right <= 0:
+            self.pos.x = 0
+        self.rect.x = round(self.pos.x)
